c1cf3670e2453cc84b624a5e4b170ad56a6c0bc3
iqboljon <abduvaliyeviqboljon99@gmail.com>
2025-03-20 13:25:21 +0500

    ...

---
+++ sayohat/src/pages/Categories.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar"
+import './All.css'
+function Categories() {
+  return (
+    <div>
+      <Navbar />
+      <div className="home-pages">
+        <h1>Explore the World</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Categories
